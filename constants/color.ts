0d0172e8d55cf01475ec16aee222476b0a7f0ce7
Cindy B. Victoria <56531858+cindyvictoria01@users.noreply.github.com>
2024-03-05 04:10:37 +0800

    Add files via upload

---
+++ constants/color.ts
@@ -0,0 +1,17 @@
+export default {
+  defaultText: "#292d2f",
+  fullblack: "#000",
+  white: "#FFF",
+
+  primary1: "#74EFD1",
+  primary2: "#04395E",
+  primary3: "#6ABFFF",
+
+  neutral1: "#C9D4DB",
+  neutral2: "#8D9EBB",
+  neutral3: "#999999",
+
+  yellow: "#FECA14",
+
+  error: "#DF6565",
+};
---
+++ constants/size.ts
@@ -0,0 +1,6 @@
+export default {
+  defaultText: 13,
+  h1: 16,
+  h2: 14,
+  h3: 13,
+};
